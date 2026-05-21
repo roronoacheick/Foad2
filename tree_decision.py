a1da5508a39a483ed583e18508c894b1d7b2760f
roronoacheick <roronoacheick3@icloud.com>
2026-05-21 21:14:41 +0200

    Feature:Add the method evaluate that will calculatethe precision,recall,f1_score and return the f1_macro to the class DecisionTree

--- tree_decision.py
+++ tree_decision.py
@@ -31,3 +31,47 @@ class DecisionTree:
             raise Exception("The tree is not trained yet. Call fit() first.")
  
         return self._traverse_tree(sample, self.root)
+    
+
+    def evaluate(self, X, Y):
+        X = [list(row) for row in X]
+        Y = list(Y)
+ 
+        unique_classes = list(set(Y))
+ 
+        all_predictions = [self.predict(sample) for sample in X]
+ 
+        f1_scores_per_class = []
+ 
+        for current_class in unique_classes:
+            true_positives  = 0   
+            false_positives = 0   
+            false_negatives = 0   
+ 
+            for predicted_class, true_class in zip(all_predictions, Y):
+                if predicted_class == current_class and true_class == current_class:
+                    true_positives += 1
+                elif predicted_class == current_class and true_class != current_class:
+                    false_positives += 1
+                elif predicted_class != current_class and true_class == current_class:
+                    false_negatives += 1
+ 
+            if true_positives + false_positives == 0:
+                precision = 0.0
+            else:
+                precision = true_positives / (true_positives + false_positives)
+ 
+            if true_positives + false_negatives == 0:
+                recall = 0.0
+            else:
+                recall = true_positives / (true_positives + false_negatives)
+ 
+            if precision + recall == 0:
+                f1_current_class = 0.0
+            else:
+                f1_current_class = 2 * (precision * recall) / (precision + recall)
+ 
+            f1_scores_per_class.append(f1_current_class)
+ 
+        f1_macro = sum(f1_scores_per_class) / len(f1_scores_per_class)
+        return f1_macro
